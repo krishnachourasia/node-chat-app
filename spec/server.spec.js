a197880ab7b3ba754584ee93b2c17f09ba44bca6
krishnachourasia <kchourasia66@gmail.com>
2020-10-06 18:53:50 +0530

    Testing Module Done

---
+++ spec/server.spec.js
@@ -0,0 +1,26 @@
+let request = require('request')
+
+
+describe('calc', () => {
+    it("should multiply 2 by 2", () => {
+        expect(2*2).toBe(4)
+    })
+})
+
+describe('get messages', () => {
+    it("should return 200 OK", (done) => {
+        request.get(`http://localhost:3000/`, (err, res) => {
+            expect(res.statusCode).toEqual(200)
+            done()
+
+        })
+    })
+
+    it("should return a list, thats not empty", (done) => {
+        request.get(`http://localhost:3000/messages`, (err, res) => {
+            expect(JSON.parse(res.body).length).toBeGreaterThan(0)
+            done()
+
+        })
+    })
+})
